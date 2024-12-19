c34542be0de377801d8293cea0b598d5cdd6fbd9
smrutidatap <smruti.sahoo@dataprowess.com.au>
2024-12-19 11:14:13 +0530

    Resolved Bugs raised by QA

--- util/config.js
+++ util/config.js
@@ -1,5 +1,6 @@
 // config.js
-const API_BASE_URL = "http://35.154.204.105/api/";
+// const API_BASE_URL = "http://35.154.204.105/api/";
 // const API_BASE_URL = "http://localhost:8000/api/";
+const API_BASE_URL = "https://bugbear.in/api/";
 
 export default API_BASE_URL;
