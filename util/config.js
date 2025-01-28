84682a1a04270fd7c9a8aa88efb388fb4c24d11b
smrutidatap <smruti.sahoo@dataprowess.com.au>
2025-01-28 12:24:57 +0530

    updated config

--- util/config.js
+++ util/config.js
@@ -3,6 +3,7 @@
 // const API_BASE_URL = "http://localhost:8000/api/";
 // Hey
 // const API_BASE_URL = "https://bugbear.in/api/";
-const API_BASE_URL = "http://74.225.248.147/api/";
+// const API_BASE_URL = "http://74.225.248.147/api/";
+const API_BASE_URL1 = "https://https://bugback-production-2362.up.railway.app/api/";
 
 export default API_BASE_URL;
