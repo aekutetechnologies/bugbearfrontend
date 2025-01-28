7effa55f4eb393b5be7c09fe737893139c323e20
smrutidatap <smruti.sahoo@dataprowess.com.au>
2025-01-28 08:48:21 +0530

    Update package.json

--- util/config.js
+++ util/config.js
@@ -2,6 +2,7 @@
 // const API_BASE_URL = "http://35.154.204.105/api/";
 // const API_BASE_URL = "http://localhost:8000/api/";
 // Hey
-const API_BASE_URL = "https://bugbear.in/api/";
+// const API_BASE_URL = "https://bugbear.in/api/";
+const API_BASE_URL = "http://74.225.248.147/api/";
 
 export default API_BASE_URL;
